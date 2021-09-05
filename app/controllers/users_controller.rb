96fe5e59700db600df8493ddc528db70f22778e8
swikriti ranjan <swikritiranjan123@gmail.com>
2021-09-05 12:27:45 +0530

    add freinds display and freindship functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,32 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friends = current_user.friend
+  end
+
+  def search
+     if params[:friend].present?
+        @friends = User.search(params[:friend])
+        @friends = current_user.except_current_user(@friends)
+        if @friends
+          respond_to do |format|
+            format.js {render partial: 'users/friend_result'}
+          end
+          
+        else
+          respond_to do |format|
+            flash.now[:alert] = "could not find user"
+            format.js {render partial: 'users/friend_result'}
+          end
+          
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "please enter a friend name or email"
+          format.js {render partial: 'users/friend_result'}
+        end
+      end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_110838) do
+ActiveRecord::Schema.define(version: 2021_09_04_133054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "freindships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_freindships_on_friend_id"
+    t.index ["user_id"], name: "index_freindships_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -46,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_09_04_110838) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "freindships", "users"
+  add_foreign_key "freindships", "users", column: "friend_id"
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
 end
